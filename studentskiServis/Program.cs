c5a6f5b0a750578240c67396f4db070a3738b6f6
gvozden98 <ognjen.gvozden98@gmail.com>
2022-05-05 00:05:16 +0200

    Prvi deo zadatka je skoro gotov, nedostaje funkcionalnost brisanja studenta i hendlovanja exceptiona za ime i prezime, ostale stvari dobro rade

--- studentskiServis/Program.cs
+++ studentskiServis/Program.cs
@@ -50,41 +50,59 @@ namespace Student {
     class Program
     {
         public static void Kreiranje(List<Student> studenti)
-        {   
-            Student s = new Student();
-            
-            Console.WriteLine("Kreiranje\n");
+        {
+            Student s = new Student();      
 
             Console.WriteLine("Ime studenta:");
-            s.Ime = Console.ReadLine();
+            s.Ime = Console.ReadLine()??"";
 
             Console.WriteLine("Prezime studenta:");
-            s.Prezime = Console.ReadLine();
+            s.Prezime = Console.ReadLine()??"";
 
             Console.WriteLine("Prosek studenta:");
             string? prosekPom;
             double prosekVred;
-            bool okParse=false;
-           ;
+            bool okParse = false;
+
             do
             {
-                
+
                 prosekPom = Console.ReadLine();
                 okParse = Double.TryParse(prosekPom, out prosekVred);
-                if (okParse == true)
+                Console.WriteLine(prosekVred);
+                if (okParse == true && prosekVred>=6d && prosekVred<=10d)
                 {
-                    s.Prosek = prosekVred;
+                    s.Prosek = (double)Math.Round(prosekVred, 2);
+
                 }
                 else
                 {
-                    Console.WriteLine("Prosek studenta pogesno unesen!\nPokusajte ponovo:");
+                    Console.WriteLine("Prosek studenta pogesno unesen! Pokusajte ponovo:"); //ovde nesto nije dobro, ne znam sta se desava ali radi
+                    okParse = false;
                 }
+
             }
             while (!okParse);
+            okParse = false;
+            int godVred;
+            do
+            {
+                Console.WriteLine("Trenutna godina studija:");
+                string? trenGodStud = Console.ReadLine();
 
+                okParse = int.TryParse(trenGodStud, out godVred);
+                if (okParse == true)
+                {
+                    s.TrenutnaGodinaStudija = godVred;
+                }
+                else
+                {
+                    Console.WriteLine("Niste uneli adekvatan broj");
+                }
 
-            Console.WriteLine("Trenutna godina studija:");
-            s.TrenutnaGodinaStudija = Convert.ToInt32(Console.ReadLine());
+            } while (!okParse || (godVred< 1 || godVred>4));
+            
+              
 
             Console.WriteLine("Datum upisa: dd/MM/yyyy");
             bool isValidDate = DateTime.TryParse(Console.ReadLine(), out s.datumUpisa);
@@ -92,12 +110,35 @@ namespace Student {
             {
                 isValidDate = DateTime.TryParse(Console.ReadLine(), out s.datumUpisa);
             }
-            Console.WriteLine("Broj indeksa u formatu: godina_studija/broj_indeksa");
-            string brojIndeksa = Console.ReadLine();
-            string[] godinaBrojIndeksa = brojIndeksa.Split('/');
-            Console.WriteLine(godinaBrojIndeksa[0]); // <--------------------- Mora da se hendla exception
-            Console.WriteLine(godinaBrojIndeksa[1]);
-            s.BrojIndeksa = new BrojIndeksa(Int32.Parse(godinaBrojIndeksa[0]), Int32.Parse(godinaBrojIndeksa[1]));
+
+            bool godParse = false;
+            bool indParse = false;
+            int godInd =0;
+            int brInd = 0;
+
+            while (!godParse)
+            {
+                Console.WriteLine("Godina indeksa:");
+                godParse = int.TryParse(Console.ReadLine(), out godInd);
+                
+            }
+            while (!indParse)
+            {
+                Console.WriteLine("Broj indeksa");
+                indParse = int.TryParse(Console.ReadLine(), out brInd);
+            }
+       
+            //provera da li postoji student 
+            foreach (Student st in studenti)
+            {
+                if (st.BrojIndeksa.broj == brInd && st.BrojIndeksa.godina == godInd)
+                {
+                    Console.WriteLine("Student sa tim brojem indeksa vec postoji u sistemu\n");
+                    return;
+                }
+                
+            }
+            s.BrojIndeksa = new BrojIndeksa(godInd, brInd);
             bool odgovor = false;
             while (!odgovor)
             {
@@ -133,17 +174,163 @@ namespace Student {
             Console.WriteLine(s.FaxSmer + "\n" + s.Ime + "\n" + s.Prezime + "\n" + s.Prosek + "\n" + s.TrenutnaGodinaStudija + "\n" + s.DatumUpisa.Date.ToString("dd-MM-yyyy"));
             studenti.Add(s);
         }
-        void PrikazJednogStudenta()
+        public static void PrikazJednogStudenta(List<Student> studenti)
         {
+            bool godParse = false;
+            bool indParse = false;
+            int godInd = 0;
+            int brInd = 0;
 
+            while (!godParse)
+            {
+                Console.WriteLine("Godina indeksa:");
+                godParse = int.TryParse(Console.ReadLine(), out godInd);
+
+            }
+            while (!indParse)
+            {
+                Console.WriteLine("Broj indeksa");
+                indParse = int.TryParse(Console.ReadLine(), out brInd);
+            }
+            foreach (Student s in studenti)
+            {
+                if (s.BrojIndeksa.godina==godInd && s.BrojIndeksa.broj==brInd)
+                {
+                    Console.WriteLine(s.Ime + "\n" + s.Prezime + "\n" + s.Prosek + "\n" + s.TrenutnaGodinaStudija + "\n" + s.DatumUpisa.Date.ToString("dd-MM-yyyy")+ "\n"+ s.BrojIndeksa.godina +$"/"+ s.BrojIndeksa.broj + "\n" + s.FaxSmer + "\n");
+                    return;
+                }
+                
+            }
+            Console.WriteLine("Trazeni student ne postoji!");
+            return;
         }
-        void PrikazSvihStudenata()
+        public static void PrikazSvihStudenata(List<Student> studenti)
         {
+            foreach (Student s in studenti)
+            {
+                Console.WriteLine(s.Ime + "\n" + s.Prezime + "\n" + s.Prosek + "\n" + s.TrenutnaGodinaStudija + "\n" + s.DatumUpisa.Date.ToString("dd-MM-yyyy")+ "\n"+ s.BrojIndeksa.godina +$"/"+ s.BrojIndeksa.broj + "\n" +s.FaxSmer + "\n");
 
+            }
+            return;
         }
 
-        void Azuriranje()
+        public static void Azuriranje(List<Student> studenti)
         {
+            bool godParse = false;
+            bool indParse = false;
+            int godInd = 0;
+            int brInd = 0;
+
+            while (!godParse)
+            {
+                Console.WriteLine("Godina indeksa:");
+                godParse = int.TryParse(Console.ReadLine(), out godInd);
+
+            }
+            while (!indParse)
+            {
+                Console.WriteLine("Broj indeksa");
+                indParse = int.TryParse(Console.ReadLine(), out brInd);
+            }
+            BrojIndeksa b = new(godInd,brInd);         
+
+            foreach (Student s in studenti)
+            {
+                if (s.BrojIndeksa.Equals(b))
+                {
+                    Console.WriteLine("Ime studenta:");
+                    s.Ime = Console.ReadLine();
+
+                    Console.WriteLine("Prezime studenta:");
+                    s.Prezime = Console.ReadLine();
+
+                    Console.WriteLine("Prosek studenta:");
+                    string? prosekPom;
+                    double prosekVred;
+                    bool okParse = false;
+
+                    do
+                    {
+
+                        prosekPom = Console.ReadLine();
+                        okParse = Double.TryParse(prosekPom, out prosekVred);
+                        Console.WriteLine(prosekVred);
+                        if (okParse == true && prosekVred >= 6d && prosekVred <= 10d)
+                        {
+                            s.Prosek = (double)Math.Round(prosekVred, 2);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Prosek studenta pogesno unesen! Pokusajte ponovo:"); //ovde nesto nije dobro, ne znam sta se desava
+                            okParse = false;
+                        }
+
+                    }
+                    while (!okParse);
+                    okParse = false;
+                    int godVred;
+                    do
+                    {
+                        Console.WriteLine("Trenutna godina studija:");
+                        string? trenGodStud = Console.ReadLine();
+
+                        okParse = int.TryParse(trenGodStud, out godVred);
+                        if (okParse == true)
+                        {
+                            s.TrenutnaGodinaStudija = godVred;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Niste uneli adekvatan broj");
+                        }
+
+                    } while (!okParse || (godVred < 1 || godVred > 4));
+
+
+
+                    Console.WriteLine("Datum upisa: dd/MM/yyyy");
+                    bool isValidDate = DateTime.TryParse(Console.ReadLine(), out s.datumUpisa);
+                    while (!isValidDate)
+                    {
+                        isValidDate = DateTime.TryParse(Console.ReadLine(), out s.datumUpisa);
+                    }
+                    
+                    bool odgovor = false;
+                    while (!odgovor)
+                    {
+                        Console.WriteLine("Smer: ISIT, ME, OM, OK");
+                        string smerText = Console.ReadLine().ToUpper();
+                        switch (smerText)
+                        {
+                            case "ISIT":
+                                s.smer = Smer.ISIT;
+                                odgovor = true;
+                                break;
+                            case "ME":
+                                s.smer = Smer.ME;
+                                odgovor = true;
+                                break;
+                            case "OM":
+                                s.smer = Smer.OM;
+                                odgovor = true;
+                                break;
+                            case "OK":
+                                s.smer = Smer.OK;
+                                odgovor = true;
+                                break;
+                            default:
+                                Console.WriteLine("Nije dobro uneto ime smera!");
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ne postoji student sa tim brojem indeksa!");
+                }
+
+            }
 
         }
 
@@ -173,13 +360,13 @@ namespace Student {
                         Kreiranje(studenti);
                         break;
                     case "2":
-                        Console.WriteLine("Prikaz jednog studenta");
+                        PrikazJednogStudenta(studenti);
                         break;
                     case "3":
-                        Console.WriteLine("Prikaz vise studenata");
+                        PrikazSvihStudenata(studenti);
                         break;
                     case "4":
-                        Console.WriteLine("Azuriranje");
+                        Azuriranje(studenti);
                         break;
                     case "5":
                         Console.WriteLine("Brisanje");
